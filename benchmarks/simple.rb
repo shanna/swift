2dbc39b87dd9adc09491f7ae60f93e57b18bea84
Bharanee Rathna <deepfryed@gmail.com>
2010-07-18 08:52:14 +1000

    updated benchmarks to test typecasting

--- benchmarks/simple.rb
+++ benchmarks/simple.rb
@@ -16,16 +16,18 @@ DataMapper.setup :default, 'postgres://127.0.0.1/swift'
 
 class User
   include DataMapper::Resource
-  property :id,    Serial
-  property :name,  String
-  property :email, String
+  property :id,         Serial
+  property :name,       String
+  property :email,      String
+  property :updated_at, Time
 end # User
 
 class SwiftUser < Swift.model do
     resource :users
-    property :id,       Integer, key: true, serial: true
-    property :name,     String
-    property :email,    String
+    property :id,         Integer, key: true, serial: true
+    property :name,       String
+    property :email,      String
+    property :updated_at, Time
   end
 end # SwiftUser
 
@@ -41,13 +43,13 @@ User.auto_migrate!
 
 Benchmark.bm(12) do |bm|
   bm.report("dm create") do
-    rows.times {|n| User.create(name: "test #{n}", email: "test@example.com") }
+    rows.times {|n| User.create(name: "test #{n}", email: "test@example.com", updated_at: Time.now) }
   end
   bm.report("dm select") do
-    iter.times { User.all.each {|m| m } }
+    iter.times { User.all.each {|m| [ m.id, m.updated_at ] } }
   end
   bm.report("dm update") do
-    iter.times { User.all.each {|m| m.update(name: "foo", email: "foo@example.com") } }
+    iter.times { User.all.each {|m| m.update(name: "foo", email: "foo@example.com", updated_at: Time.now) } }
   end
 end
 
@@ -55,13 +57,13 @@ User.auto_migrate!
 
 Benchmark.bm(12) do |bm|
   bm.report("ar create") do
-    rows.times {|n| ARUser.create(name: "test #{n}", email: "test@example.com") }
+    rows.times {|n| ARUser.create(name: "test #{n}", email: "test@example.com", updated_at: Time.now) }
   end
   bm.report("ar select") do
-    iter.times {|n| ARUser.find(:all).each {|m| m } }
+    iter.times {|n| ARUser.find(:all).each {|m| [ m.id, m.updated_at ] } }
   end
   bm.report("ar update") do
-    iter.times {|n| ARUser.find(:all).each {|m| m.update_attributes(name: "foo", email: "foo@example.com") } }
+    iter.times {|n| ARUser.find(:all).each {|m| m.update_attributes(name: "foo", email: "foo@example.com", updated_at: Time.now) } }
   end
 end
 
@@ -69,12 +71,12 @@ User.auto_migrate!
 
 Benchmark.bm(12) do |bm|
   bm.report("swift create") do
-    rows.times {|n| SwiftUser.create(name: "test #{n}", email: "test@example.com") }
+    rows.times {|n| SwiftUser.create(name: "test #{n}", email: "test@example.com", updated_at: Time.now) }
   end
   bm.report("swift select") do
-    iter.times {|n| SwiftUser.all.each {|m| m } }
+    iter.times {|n| SwiftUser.all.each {|m| [ m.id, m.updated_at ] } }
   end
   bm.report("swift update") do
-    iter.times {|n| SwiftUser.all.each {|m| m.update(name: "foo", email: "foo@example.com") } }
+    iter.times {|n| SwiftUser.all.each {|m| m.update(name: "foo", email: "foo@example.com", updated_at: Time.now) } }
   end
 end
