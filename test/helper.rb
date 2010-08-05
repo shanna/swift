2fee919ac806d8d67f79bb8379f01dca298f61e7
Bharanee Rathna <deepfryed@gmail.com>
2010-08-05 22:28:01 +1000

    helper #supported_by fix

--- test/helper.rb
+++ test/helper.rb
@@ -11,13 +11,19 @@ end
 class MiniTest::Spec
   def self.supported_by *adapters, &block
     adapters.each do |adapter|
+      # test if adapter can be loaded.
       begin
         Swift.setup :default, adapter, db: 'swift_test'
       rescue => error
         warn "Unable to setup 'swift_test' db for #{adapter}, #{error.message}. Skipping..."
         next
       end
-      describe("Adapter #{adapter.name}"){ block.call(adapter) }
+      describe("Adapter #{adapter.name}") do
+        before do
+          Swift.setup :default, adapter, db: 'swift_test'
+        end
+        block.call(adapter)
+      end
     end
   end
 end
