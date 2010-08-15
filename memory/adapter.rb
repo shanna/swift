1c3b03643109e1da39f4bc218d8b30870f790929
Shane Hanna <shane.hanna@gmail.com>
2010-08-15 13:54:11 +1000

    Memory check cleanup.

--- memory/adapter.rb
+++ memory/adapter.rb
@@ -10,32 +10,28 @@ rows = (ARGV[1] || 500).to_i
 iter = (ARGV[2] ||   5).to_i
 
 50.times do |r|
-  puts ""
-  puts "-- run #{r} --"
-  puts ""
-
+  puts '', "-- run #{r} --", ''
   puts 'virt: %skB res: %skB' % `ps -o "vsize= rss=" -p #{$$}`.strip.split(/\s+/)
 
   h.execute 'drop table if exists users'
   h.execute 'create table users(id serial, name text, email text, updated_at timestamp)'
 
-  ins = h.prepare 'insert into users(name, email, updated_at) values (?, ?, ?)'
-  rows.times {|n| ins.execute("test #{n}", "test@example.com", Time.now) }
-  ins.finish
+  insert = h.prepare 'insert into users(name, email, updated_at) values (?, ?, ?)'
+  rows.times{|n| insert.execute("test #{n}", "test@example.com", Time.now) }
+  insert.finish
 
-  sel = h.prepare 'select * from users'
-  iter.times {|n| sel.execute {|m| [ m[:id], m[:name], m[:email], m[:updated_at ] ] } }
+  select = h.prepare 'select * from users'
+  iter.times{|n| select.execute{|m| [ m[:id], m[:name], m[:email], m[:updated_at ] ] } }
 
-  upd = h.prepare 'update users set name = ?, email = ?, updated_at = ? where id = ?'
-  iter.times {|n| sel.execute {|m| upd.execute("foo", "foo@example.com", Time.now, m[:id]) } }
+  update = h.prepare 'update users set name = ?, email = ?, updated_at = ? where id = ?'
+  iter.times{|n| select.execute{|m| update.execute("foo", "foo@example.com", Time.now, m[:id]) } }
 
   puts 'virt: %skB res: %skB' % `ps -o "vsize= rss=" -p #{$$}`.strip.split(/\s+/)
 
-  ins.finish
-  sel.finish
-  upd.finish
+  insert.finish
+  select.finish
+  update.finish
 
   GC.start
-
   puts 'virt: %skB res: %skB' % `ps -o "vsize= rss=" -p #{$$}`.strip.split(/\s+/)
 end
--- memory/scheme.rb
+++ memory/scheme.rb
@@ -1,10 +1,12 @@
 #!/usr/bin/ruby
 
-require 'etc'
-require_relative '../lib/swift'
+$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
+require 'swift'
+require 'swift/migrations'
 
 adapter = ARGV.first =~ /mysql/i ? Swift::DB::Mysql : Swift::DB::Postgres
-Swift.setup :default, adapter, user: Etc.getlogin, db: 'swift'
+Swift.setup :default, adapter, db: 'swift'
 
 class User < Swift::Scheme
   store     :users
@@ -18,28 +20,18 @@ rows = (ARGV[1] || 500).to_i
 iter = (ARGV[2] ||   5).to_i
 
 50.times do |r|
-  puts ""
-  puts "-- run #{r} --"
-  puts ""
-
+  puts '', "-- run #{r} --", ''
   puts 'virt: %skB res: %skB' % `ps -o "vsize= rss=" -p #{$$}`.strip.split(/\s+/)
 
   User.migrate!
-  rows.times {|n| User.create(name: "test #{n}", email: "test@example.com", updated_at: Time.now) }
-  iter.times {|n| User.all.each{|m| [ m.id, m.name, m.email, m.updated_at ] } }
-  iter.times {|n| User.all.each{|m| m.update(name: "foo", email: "foo@example.com", updated_at: Time.now) } }
+  rows.times{|n| User.create(name: "test #{n}", email: "test@example.com", updated_at: Time.now) }
+  iter.times{|n| User.all.each{|m| [ m.id, m.name, m.email, m.updated_at ] } }
+  iter.times{|n| User.all.each{|m| m.update(name: "foo", email: "foo@example.com", updated_at: Time.now) } }
 
   User.migrate!
-  n = 0
-  Swift.db.write("users", *%w{name email updated_at}) do
-    data = n < rows ? "test #{n}\ttest@example.com\t#{Time.now}\n" : nil
-    n += 1
-    data
-  end
-
+  rows.times{|n| Swift.db.write('users', %w{name email updated_at}, "test #{n}\ttest@example.com\t#{Time.now}\n") }
   puts 'virt: %skB res: %skB' % `ps -o "vsize= rss=" -p #{$$}`.strip.split(/\s+/)
 
   GC.start
-
   puts 'virt: %skB res: %skB' % `ps -o "vsize= rss=" -p #{$$}`.strip.split(/\s+/)
 end
