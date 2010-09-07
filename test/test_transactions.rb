fe83cbf6fd422d7b184222d2889e1ac176a7748d
Bharanee Rathna <deepfryed@gmail.com>
2010-09-07 17:11:54 +1000

    bumped dbic++ dependency, added test for db2 transactions

--- test/test_transactions.rb
+++ test/test_transactions.rb
@@ -1,12 +1,12 @@
 require_relative 'helper'
 describe 'Adapter' do
-  supported_by Swift::DB::Postgres, Swift::DB::Mysql do
+  supported_by Swift::DB::Postgres, Swift::DB::Mysql, Swift::DB::DB2 do
     describe 'transactions' do
       before do
         @name = 'test1 - transaction 1'
         @db   = Swift.db
-        @db.execute %q{drop table if exists users}
-        @db.execute %q{create table users(id serial, name text, created_at timestamp)}
+        @db.execute %q{drop table users} rescue nil
+        @db.execute %q{create table users(name varchar(512), created_at timestamp)}
       end
 
       it 'yields db to block' do
@@ -38,6 +38,19 @@ describe 'Adapter' do
         it 'should autocommit' do
           @db.transaction do |db|
             db.execute('insert into users(name) values(?)', @name)
+          end
+        end
+
+        it 'should autocommit and autorollback' do
+          @db.transaction do |db|
+            db.execute('insert into users(name) values(?)', @name)
+            begin
+              db.transaction do
+                db.execute('insert into users(name) values(?)', @name)
+                raise 'foo'
+              end
+            rescue RuntimeError
+            end
           end
         end
       end
