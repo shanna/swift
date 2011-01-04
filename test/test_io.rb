032d6f73b2f46c5e939f3369efba08b95f1a8f7f
Bharanee Rathna <deepfryed@gmail.com>
2011-01-04 15:48:11 +1100

    test mysql adapter execute io

--- test/test_io.rb
+++ test/test_io.rb
@@ -13,10 +13,26 @@ describe 'Adapter' do
         Swift.db.migrate! user
       end
 
-      it 'stores and retrieves an image' do
+      it 'stores and retrieves an image via prepared statement' do
         Swift.db do |db|
           io = File.open(File.dirname(__FILE__) + '/house-explode.jpg')
           db.prepare('insert into users (name, image) values(?, ?)').execute('test', io)
+
+          blob = db.prepare('select image from users limit 1').execute.first[:image]
+
+          io.rewind
+          assert_kind_of StringIO, blob
+
+          data = blob.read
+          assert_equal Encoding::ASCII_8BIT, data.encoding
+          assert_equal io.read.force_encoding('ASCII-8BIT'), data
+        end
+      end
+
+      it 'stores and retrieves an image via Adapter#execute' do
+        Swift.db do |db|
+          io = File.open(File.dirname(__FILE__) + '/house-explode.jpg')
+          db.execute('insert into users (name, image) values(?, ?)', 'test', io)
 
           blob = db.prepare('select image from users limit 1').execute.first[:image]
 
