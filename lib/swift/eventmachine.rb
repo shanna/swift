5278e43b6cd3cf904a43cf20bdfe7d1f8dd94409
Bharanee Rathna <deepfryed@gmail.com>
2012-07-21 09:14:52 +1000

    eventmachine & synchrony docs

--- lib/swift/eventmachine.rb
+++ lib/swift/eventmachine.rb
@@ -2,6 +2,9 @@ require 'eventmachine'
 require 'swift'
 
 module Swift
+  # Eventmachine Adapter Extensions.
+  #
+  # This replaces the Adapter#execute method with a non-blocking asynchronous version.
   class Adapter
     alias :blocking_execute :execute
 
@@ -22,6 +25,18 @@ module Swift
       end
     end
 
+    # Execute a command asynchronously.
+    #
+    # @example
+    #   defer = Swift.db.execute(User, "select * from users where id = ?", 1)
+    #   defer.callback do |user|
+    #     p user.id
+    #   end
+    #   defer.errback do |error|
+    #     p error
+    #   end
+    #
+    # @see  [Swift::Adapter]
     def execute command, *bind
       start = Time.now
       record, command = command, bind.shift if command.kind_of?(Class) && command < Record
